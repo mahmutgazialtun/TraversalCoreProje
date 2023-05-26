e93624ead7700543417a8ff147a7ef3491d2c592
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2023-05-26 14:22:08 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-MN15E09\\; database=TraversalDB; integrated security=true;");
+        }
+              // <sınıfın adı> tablonun adı
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> Abou2ts { get; set; }
+        public DbSet<Contect> Contects { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Feature2s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
