6ee15328fdf78b951d877945f0adf95e199b2f95
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2023-05-26 23:42:37 +0300

    14.Ders Sonu

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -13,7 +13,7 @@ namespace EntityLayer.Concrete
         public int AboutID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
+        public string Image1 { get; set; }
         public string Title2 { get; set; }
         public string Description2 { get; set; }
         public bool Status { get; set; }
