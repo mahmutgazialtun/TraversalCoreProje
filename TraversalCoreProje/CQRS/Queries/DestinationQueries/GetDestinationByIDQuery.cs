6142b71d29412784b455df120d2863dc4564e33b
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2023-07-14 01:06:56 +0300

    73.Ders sonu CQRS tasarım deseni eklendi

---
+++ TraversalCoreProje/CQRS/Queries/DestinationQueries/GetDestinationByIDQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.CQRS.Queries.DestinationQueries
+{
+    public class GetDestinationByIDQuery
+    {
+        public GetDestinationByIDQuery(int id)
+        {
+            this.id = id;
+        }
+
+        //parametrelerin gönderileceği sınıf
+
+        public int id { get; set; }
+    }
+}
