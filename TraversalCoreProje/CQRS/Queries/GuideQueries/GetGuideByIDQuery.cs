dc312e44bf03df16ae43e6e42e32d01be19696f0
Mahmut Gazi Altun <mahmutaltun535@gmail.com>
2023-07-30 23:23:09 +0300

    CQRS Guide

---
+++ TraversalCoreProje/CQRS/Queries/GuideQueries/GetGuideByIDQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.CQRS.Results.GuideResults;
+
+namespace TraversalCoreProje.CQRS.Queries.GuideQueries
+{
+    public class GetGuideByIDQuery:IRequest<GetGuideByIDQueryResult>
+    {
+        public GetGuideByIDQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
